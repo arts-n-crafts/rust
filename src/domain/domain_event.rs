26bc5012f2859fdb6ffb1b86e989a60c61a7222f
Jens van Hellemondt <jens@invictus.codes>
2025-09-13 20:38:00 +0200

    feat: DomainEvent

---
+++ src/domain/domain_event.rs
@@ -0,0 +1,100 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum EventSource {
+    Internal,
+    External,
+}
+
+pub struct DomainEvent<TPayload: Serialize + Send + Sync + Clone> {
+    id: Uuid,
+    name: String,
+    aggregate_id: Uuid,
+    source: EventSource,
+    payload: TPayload,
+    timestamp: NaiveDateTime,
+    metadata: HashMap<String, String>,
+}
+
+impl<TPayload: Serialize + Send + Sync + Clone> DomainEvent<TPayload> {
+    pub fn create(name: String, aggregate_id: Uuid, payload: TPayload) -> Self {
+        DomainEvent {
+            id: Uuid::now_v7(),
+            name,
+            aggregate_id,
+            source: EventSource::Internal,
+            payload,
+            timestamp: Default::default(),
+            metadata: HashMap::new(),
+        }
+    }
+
+    pub fn add_to_metadata(&mut self, key: String, value: String) {
+        self.metadata.insert(key, value);
+    }
+
+    pub fn set_correlation_id(&mut self, correlation_id: String) {
+        self.metadata
+            .insert("correlation_id".to_string(), correlation_id);
+    }
+
+    pub fn set_causation_id(&mut self, causation_id: String) {
+        self.metadata
+            .insert("causation_id".to_string(), causation_id);
+    }
+}
+
+#[cfg(test)]
+mod create_domain_event_tests {
+    use super::*;
+    use rstest::{fixture, rstest};
+    use serde::{Deserialize, Serialize};
+    use uuid::Uuid;
+
+    #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+    struct User {
+        id: u8,
+        name: String,
+    }
+
+    #[fixture]
+    fn fixture() -> (DomainEvent<User>, String, Uuid, User) {
+        let event_name = "user_created".to_string();
+        let aggregate_id = Uuid::now_v7();
+        let payload: User = User {
+            id: 1,
+            name: "John".to_string(),
+        };
+        let event = DomainEvent::create(event_name.clone(), aggregate_id.clone(), payload.clone());
+        (event, event_name, aggregate_id, payload)
+    }
+
+    #[rstest]
+    fn it_should_create_a_domain_event(fixture: (DomainEvent<User>, String, Uuid, User)) {
+        let (event, event_name, aggregate_id, payload) = fixture;
+        assert_eq!(event.name, event_name);
+        assert_eq!(event.aggregate_id, aggregate_id);
+        assert_eq!(event.source, EventSource::Internal);
+        assert_eq!(event.payload, payload);
+        assert_eq!(event.metadata, HashMap::new());
+    }
+
+    #[rstest]
+    fn it_should_add_metadata_causation_id_and_correlation_id(
+        fixture: (DomainEvent<User>, String, Uuid, User),
+    ) {
+        let mut event = fixture.0;
+        let causation_id = Uuid::now_v7();
+        let correlation_id = Uuid::now_v7();
+        event.set_causation_id(causation_id.clone().to_string());
+        event.set_correlation_id(correlation_id.clone().to_string());
+        event.add_to_metadata("version".to_string(), "1".to_string());
+
+        assert_eq!(*event.metadata.get("correlation_id").unwrap(), correlation_id.to_string());
+        assert_eq!(*event.metadata.get("causation_id").unwrap(), causation_id.to_string());
+        assert_eq!(*event.metadata.get("version").unwrap(), "1".to_string());
+    }
+}
