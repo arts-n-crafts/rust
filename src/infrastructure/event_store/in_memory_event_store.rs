ec03b2952e97cba4f897a099c78d0ddd8f361659
Jens van Hellemondt <jens@invictus.codes>
2025-09-14 20:53:59 +0200

    feat(EventStore): InMemoryEventStore

---
+++ src/infrastructure/event_store/in_memory_event_store.rs
@@ -0,0 +1,117 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use serde::Serialize;
+use tokio::sync::Mutex;
+use crate::infrastructure::event_store::stored_event::StoredEvent;
+use crate::infrastructure::event_store::stream_key::StreamKey;
+
+#[derive(Debug, PartialEq)]
+pub enum EventStoreError {
+    Unreachable,
+}
+
+
+pub struct InMemoryEventStore<TEvent: Serialize + Send + Sync + Clone> {
+    data: Arc<Mutex<HashMap<StreamKey, Vec<StoredEvent<TEvent>>>>>,
+    is_offline: bool,
+}
+
+impl<TEvent: Serialize + Send + Sync + Clone> InMemoryEventStore<TEvent> {
+    pub fn new() -> Self {
+        InMemoryEventStore {
+            is_offline: false,
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn go_offline(&mut self) {
+        self.is_offline = true
+    }
+
+    pub async fn store(&self, key: StreamKey, value: TEvent) -> Result<(), EventStoreError> {
+        if self.is_offline {
+            return Err(EventStoreError::Unreachable);
+        }
+        let stored_event = StoredEvent::new(key.clone(), 1, value);
+        let mut data = self.data.lock().await;
+        data.entry(key)
+            .or_insert_with(Vec::new)
+            .push(stored_event);
+        Ok(())
+    }
+
+    pub async fn query(&self, stream_key: StreamKey) -> Result<Vec<StoredEvent<TEvent>>, EventStoreError> {
+        if self.is_offline {
+            return Err(EventStoreError::Unreachable);
+        }
+
+        let data = self.data.lock().await;
+        let result = data.get(&stream_key).map(|v| v.clone()).unwrap_or_default();
+        Ok(result)
+    }
+}
+
+
+#[cfg(test)]
+mod in_memory_database {
+    use super::*;
+    use rstest::{fixture, rstest};
+    use uuid::Uuid;
+    use crate::domain::domain_event::DomainEvent;
+
+    #[derive(Serialize, PartialEq, Debug, Clone)]
+    struct TestPayload {
+        id: Uuid,
+    }
+
+    #[fixture]
+    fn user_created_event() -> DomainEvent<'static, TestPayload> {
+        let aggregate_id = Uuid::now_v7();
+        let payload = TestPayload {
+            id: Uuid::now_v7()
+        };
+        DomainEvent::create("UserCreated", aggregate_id, payload)
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn should_store_the_data(user_created_event: DomainEvent<'static, TestPayload>) {
+        let event_store = InMemoryEventStore::new();
+        let stream_key = StreamKey::new("users", user_created_event.aggregate_id);
+        let result = event_store.store(stream_key, user_created_event).await;
+        assert!(result.is_ok());
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn should_fail_storing_if_event_store_is_offline(user_created_event: DomainEvent<'static, TestPayload>) {
+        let mut event_store = InMemoryEventStore::new();
+        event_store.go_offline();
+        let stream_key = StreamKey::new("users", user_created_event.aggregate_id);
+        let result = event_store.store(stream_key, user_created_event).await;
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), EventStoreError::Unreachable);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn should_query_the_data(user_created_event: DomainEvent<'static, TestPayload>) {
+        let event_store = InMemoryEventStore::new();
+        let stream_key = StreamKey::new("users", user_created_event.aggregate_id);
+        event_store.store(stream_key.clone(), user_created_event).await.expect("unexpected store failed");
+
+        let result = event_store.query(stream_key).await;
+        assert!(result.is_ok());
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn should_fail_querying_if_database_is_offline() {
+        let mut event_store = InMemoryEventStore::<StoredEvent<DomainEvent<'static, TestPayload>>>::new();
+        event_store.go_offline();
+        let stream_key = StreamKey::new("users", Uuid::now_v7());
+        let result = event_store.query(stream_key).await;
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(),EventStoreError::Unreachable);
+    }
+}
