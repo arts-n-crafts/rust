69c1cd7c277924a779bb864c7af7492b9e41da23
Jens van Hellemondt <jens@invictus.codes>
2025-09-17 22:34:17 +0200

    feat: command handler

--- src/core/command_handler.rs
+++ src/core/command_handler.rs
@@ -1,1 +1,70 @@
+use crate::core::base_payload::BasePayload;
+use crate::core::command::Command;
+use async_trait::async_trait;
+use serde::Serialize;
 
+#[async_trait]
+pub trait CommandHandler<TPayload, TResult, TError>
+where
+    TPayload: BasePayload + AsRef<str>,
+    TResult: Serialize + Send + Sync + Clone,
+{
+    async fn execute(&self, a_command: Command<TPayload>) -> Result<TResult, TError>;
+}
+
+#[cfg(test)]
+mod command_handler_tests {
+    use super::*;
+    use rstest::rstest;
+    use serde::{Deserialize, Serialize};
+    use strum_macros::AsRefStr;
+    use uuid::Uuid;
+
+    #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+    struct User {
+        pub id: String,
+        pub name: String,
+        pub likes: u8,
+    }
+
+    #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+    struct UserCommandResult {
+        id: String,
+    }
+
+    #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, AsRefStr)]
+    enum UserCommandPayload {
+        CreateUser { name: String },
+        LikeUser,
+    }
+
+    struct CreateUserCommandHandler;
+
+    #[async_trait]
+    impl CommandHandler<UserCommandPayload, UserCommandResult, ()> for CreateUserCommandHandler {
+        async fn execute(
+            &self,
+            a_command: Command<UserCommandPayload>,
+        ) -> Result<UserCommandResult, ()> {
+            Ok(UserCommandResult {
+                id: a_command.aggregate_id,
+            })
+        }
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn it_should_return_the_id_of_the_created_user() {
+        let an_aggregate_id = Uuid::now_v7().to_string();
+        let a_payload = UserCommandPayload::CreateUser {
+            name: "John Doe".to_string(),
+        };
+        let a_command = Command::create(an_aggregate_id.clone(), a_payload);
+        let a_command_handler = CreateUserCommandHandler {};
+        let a_result = a_command_handler
+            .execute(a_command)
+            .await
+            .expect("failed to execute command");
+        assert_eq!(a_result.id, an_aggregate_id);
+    }
+}
