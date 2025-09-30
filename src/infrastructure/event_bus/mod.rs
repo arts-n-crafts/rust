5ddd0e33f645ad96fafbc6c8cd5532c2690d695e
Jens van Hellemondt <jens@invictus.codes>
2025-09-30 18:48:11 +0200

    chore(EventBus): make EventProducer public

--- src/infrastructure/event_bus/mod.rs
+++
@@ -1,2 +0,0 @@
-mod event_producer;
-mod in_memory_event_producer;
--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,5 +1,10 @@
-mod event_bus;
+pub mod event_bus {
+    pub mod event_producer;
+    pub mod in_memory_event_producer;
+}
+
 pub mod event_store;
+
 pub mod database {
     pub mod database_query;
     pub mod in_memory_database;
